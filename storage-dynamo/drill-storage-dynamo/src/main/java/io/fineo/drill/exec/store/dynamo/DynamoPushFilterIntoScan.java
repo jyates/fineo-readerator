e7174703daaf2a46c1d80c596b0460cb5da50334
Jesse Yates <jesse.k.yates@gmail.com>
2016-07-12 16:07:33 -0700

    Adding skeleton of filtering from HBase and better pk tracking

---
+++ storage-dynamo/drill-storage-dynamo/src/main/java/io/fineo/drill/exec/store/dynamo/DynamoPushFilterIntoScan.java
@@ -0,0 +1,154 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.fineo.drill.exec.store.dynamo;
+
+import com.google.common.collect.ImmutableList;
+import org.apache.calcite.plan.RelOptRuleCall;
+import org.apache.calcite.plan.RelOptRuleOperand;
+import org.apache.calcite.plan.RelOptUtil;
+import org.apache.calcite.rel.RelNode;
+import org.apache.calcite.rex.RexNode;
+import org.apache.drill.common.expression.LogicalExpression;
+import org.apache.drill.exec.physical.base.GroupScan;
+import org.apache.drill.exec.planner.logical.DrillOptiq;
+import org.apache.drill.exec.planner.logical.DrillParseContext;
+import org.apache.drill.exec.planner.logical.RelOptHelper;
+import org.apache.drill.exec.planner.physical.FilterPrel;
+import org.apache.drill.exec.planner.physical.PrelUtil;
+import org.apache.drill.exec.planner.physical.ProjectPrel;
+import org.apache.drill.exec.planner.physical.ScanPrel;
+import org.apache.drill.exec.store.StoragePluginOptimizerRule;
+
+public final class DynamoPushFilterIntoScan {
+
+  private DynamoPushFilterIntoScan() {
+  }
+
+  private abstract static class FilterIntoScanBase extends StoragePluginOptimizerRule {
+
+    private FilterIntoScanBase(RelOptRuleOperand operand, String description) {
+      super(operand, description);
+    }
+
+    @Override
+    public boolean matches(RelOptRuleCall call) {
+      final ScanPrel scan = call.rel(1);
+      if (scan.getGroupScan() instanceof DynamoGroupScan) {
+        return super.matches(call);
+      }
+      return false;
+    }
+
+    @Override
+    public final void onMatch(RelOptRuleCall call) {
+      final ScanPrel scan = call.rel(1);
+      // according to Drill docs we ned to check again even after the matches check
+      GroupScan gs = scan.getGroupScan();
+      if (!(gs instanceof DynamoGroupScan)) {
+        return;
+      }
+      DynamoGroupScan groupScan = (DynamoGroupScan) gs;
+      if (groupScan.isFilterPushedDown()) {
+        /*
+         * The rule can get triggered again due to the transformed "scan => filter" sequence
+         * created by the earlier execution of this rule when we could not do a complete
+         * conversion of Optiq Filter's condition to HBase Filter. In such cases, we rely upon
+         * this flag to not do a re-processing of the rule on the already transformed call.
+         */
+        return;
+      }
+      pushdown(call);
+    }
+
+    protected abstract void pushdown(RelOptRuleCall call);
+  }
+
+  public static final StoragePluginOptimizerRule FILTER_ON_SCAN = new FilterIntoScanBase(
+    RelOptHelper.some(FilterPrel.class, RelOptHelper.any(ScanPrel.class)),
+    "DynamoPushFilterIntoScan:Filter_On_Scan") {
+
+    @Override
+    protected void pushdown(RelOptRuleCall call) {
+      final FilterPrel filter = call.rel(0);
+      final RexNode condition = filter.getCondition();
+
+      final ScanPrel scan = call.rel(1);
+      DynamoGroupScan groupScan = (DynamoGroupScan) scan.getGroupScan();
+      doPushFilterToScan(call, filter, null, scan, groupScan, condition);
+    }
+  };
+
+
+  public static final StoragePluginOptimizerRule FILTER_ON_PROJECT = new FilterIntoScanBase(
+    RelOptHelper
+      .some(FilterPrel.class,
+        RelOptHelper.some(ProjectPrel.class, RelOptHelper.any(ScanPrel.class))),
+    "DynamoPushFilterIntoScan:Filter_On_Project") {
+
+    @Override
+    protected void pushdown(RelOptRuleCall call) {
+      final FilterPrel filter = call.rel(0);
+      final ProjectPrel project = call.rel(1);
+      final ScanPrel scan = call.rel(2);
+
+      // convert the filter to one that references the child of the project
+      final RexNode condition = RelOptUtil.pushFilterPastProject(filter.getCondition(), project);
+      doPushFilterToScan(call, filter, project, scan, (DynamoGroupScan) scan.getGroupScan(),
+        condition);
+    }
+  };
+
+
+  private static void doPushFilterToScan(final RelOptRuleCall call, final FilterPrel filter, final
+  ProjectPrel project, final ScanPrel scan, final DynamoGroupScan groupScan,
+    final RexNode condition) {
+
+    final LogicalExpression conditionExp = DrillOptiq
+      .toDrill(new DrillParseContext(PrelUtil.getPlannerSettings(call.getPlanner())), scan,
+        condition);
+    final DynamoFilterBuilder filterBuilder = new DynamoFilterBuilder(groupScan, conditionExp);
+    final DynamoScanSpec newScanSpec = filterBuilder.parseTree();
+    if (newScanSpec == null) {
+      return; //no filter pushdown ==> No transformation.
+    }
+
+    final DynamoGroupScan newGroupsScan = new DynamoGroupScan(groupScan.getUserName(), groupScan
+      .getStoragePlugin(), newScanSpec, groupScan.getColumns());
+    newGroupsScan.setFilterPushedDown(true);
+
+    final ScanPrel newScanPrel =
+      ScanPrel.create(scan, filter.getTraitSet(), newGroupsScan, scan.getRowType());
+
+    // Depending on whether is a project in the middle, assign either scan or copy of project to
+    // childRel.
+    final RelNode childRel = project == null ? newScanPrel : project
+      .copy(project.getTraitSet(), ImmutableList.of((RelNode) newScanPrel)); ;
+
+    if (filterBuilder.isAllExpressionsConverted()) {
+        /*
+         * Since we could convert the entire filter condition expression into an HBase filter,
+         * we can eliminate the filter operator altogether.
+         */
+      call.transformTo(childRel);
+    } else {
+      call.transformTo(filter.copy(filter.getTraitSet(), ImmutableList.of(childRel)));
+    }
+  }
+
+}
--- storage-dynamo/drill-storage-dynamo/src/main/java/io/fineo/drill/exec/store/dynamo/DynamoTableDefinition.java
+++ storage-dynamo/drill-storage-dynamo/src/main/java/io/fineo/drill/exec/store/dynamo/DynamoTableDefinition.java
@@ -4,20 +4,20 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 
+import java.util.List;
 import java.util.Map;
 
 @JsonTypeName("dynamo-table")
 public class DynamoTableDefinition {
 
   private final String name;
-  private final Map<String, String> pkToType;
+  private final List<PrimaryKey> keys;
 
   @JsonCreator
   public DynamoTableDefinition(@JsonProperty("name") String name,
-    @JsonProperty("pkToType") Map<String, String>
-    pkToType) {
+    @JsonProperty("keys") List<PrimaryKey> keys) {
     this.name = name;
-    this.pkToType = pkToType;
+    this.keys = keys;
   }
 
   @JsonProperty
@@ -26,7 +26,38 @@ public class DynamoTableDefinition {
   }
 
   @JsonProperty
-  public Map<String, String> getPkToType() {
-    return pkToType;
+  public List<PrimaryKey> getKeys() {
+    return keys;
+  }
+
+  @JsonTypeName("dynamo-key-def")
+  public static class PrimaryKey {
+    private final String name;
+    private String type;
+    private final boolean isHashKey;
+
+    @JsonCreator
+    public PrimaryKey(@JsonProperty("name") String name, @JsonProperty("type") String type,
+      @JsonProperty("isHashKey") boolean isHashKey) {
+      this.name = name;
+      this.type = type;
+      this.isHashKey = isHashKey;
+    }
+
+    public String getName() {
+      return name;
+    }
+
+    public String getType() {
+      return type;
+    }
+
+    public boolean isHashKey() {
+      return isHashKey;
+    }
+
+    public void setType(String type) {
+      this.type = type;
+    }
   }
 }
